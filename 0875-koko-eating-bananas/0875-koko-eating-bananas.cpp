77c04ad29162cf4a2b8e76180249c902934406a1
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-02-20 21:19:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxEle(vector<int>&piles){
+        int maxi=INT_MIN;
+        for(int i=0;i<piles.size();i++){
+            maxi=max(maxi,piles[i]);
+        }
+        return maxi;
+    }
+
+    int func(vector<int>&piles,int mid){
+        long long total=0;
+        for(int i=0;i<piles.size();i++){
+            total+=(piles[i]+mid-1)/mid;            
+        }
+        return total;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        long long low=1;
+        long long high=maxEle(piles);
+        long long ans=high;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+
+            long long totalHrs=func(piles,mid);
+            if(totalHrs<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }   
+        return ans;  
+    }
+};
