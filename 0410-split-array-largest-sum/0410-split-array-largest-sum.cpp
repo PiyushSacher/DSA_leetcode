c1919b969ab8a2aad3fe6acbca419f4f34b72e11
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-03-02 15:56:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (23.61%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -40,14 +40,14 @@ public:
         while(low<=high){
             int mid=low+(high-low)/2;
             int noOfPainters=fun(nums,mid);
-            if(noOfPainters==k){
+            if(noOfPainters<=k){
                 ans=mid;
                 high=mid-1;
             }
-            else if(fun(nums,mid)>k){
+            else{
                 low=mid+1;
             }
-            else high=mid-1;
+            
         }
         return ans;
 
