901d16db0ad98fabe4d1b0a606b5192b0f01ab84
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-03-28 15:00:32 +0530

    Time: 4 ms (0.65%), Space: 13.2 MB (5.39%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,33 +11,33 @@
  */
 class Solution {
 public: 
-    // vector<int>v;
-    // vector<int> postorderTraversal(TreeNode* root) {
-    //     if(root==NULL) return v;
-    //     postorderTraversal(root->left);
-    //     postorderTraversal(root->right);
-    //     v.push_back(root->val);
-    //     return v;
-    // }
+    vector<int>v;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root==NULL) return v;
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        v.push_back(root->val);
+        return v;
+    }
 
    
-    vector<int> postorderTraversal(TreeNode* root){
-        vector<int>v;
-        if(root==NULL) return v;
-        stack<TreeNode*> st1,st2;
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* curr=st1.top();
-            st1.pop();
-            st2.push(curr);
-            if(curr->left) st1.push(curr->left);
-            if(curr->right) st1.push(curr->right);
-        }
-        while(!st2.empty()){
-            v.push_back((st2.top())->val);
-            st2.pop();
-        }
-        return v;
+    // vector<int> postorderTraversal(TreeNode* root){
+    //     vector<int>v;
+    //     if(root==NULL) return v;
+    //     stack<TreeNode*> st1,st2;
+    //     st1.push(root);
+    //     while(!st1.empty()){
+    //         TreeNode* curr=st1.top();
+    //         st1.pop();
+    //         st2.push(curr);
+    //         if(curr->left) st1.push(curr->left);
+    //         if(curr->right) st1.push(curr->right);
+    //     }
+    //     while(!st2.empty()){
+    //         v.push_back((st2.top())->val);
+    //         st2.pop();
+    //     }
+    //     return v;
 
-    }
+    // }
 };
