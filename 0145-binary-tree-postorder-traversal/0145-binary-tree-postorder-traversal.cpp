f63d3f7d735b3cb10bec61a59b1487f8e24ba341
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-03-29 20:15:20 +0530

    Time: 0 ms (100%), Space: 11 MB (44.1%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -10,34 +10,36 @@
  * };
  */
 class Solution {
-public: 
-    vector<int>v;
-    vector<int> postorderTraversal(TreeNode* root) {
+public:
+    vector<int> postorderTraversal(TreeNode* root){
+        vector<int>v;
         if(root==NULL) return v;
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
-        v.push_back(root->val);
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* curr=st1.top();
+            st1.pop();
+            st2.push(curr);
+            if(curr->left) st1.push(curr->left);
+            if(curr->right) st1.push(curr->right);
+        }
+        while(!st2.empty()){
+            v.push_back((st2.top())->val);
+            st2.pop();
+        }
         return v;
+
     }
 
-   
-    // vector<int> postorderTraversal(TreeNode* root){
-    //     vector<int>v;
-    //     if(root==NULL) return v;
-    //     stack<TreeNode*> st1,st2;
-    //     st1.push(root);
-    //     while(!st1.empty()){
-    //         TreeNode* curr=st1.top();
-    //         st1.pop();
-    //         st2.push(curr);
-    //         if(curr->left) st1.push(curr->left);
-    //         if(curr->right) st1.push(curr->right);
-    //     }
-    //     while(!st2.empty()){
-    //         v.push_back((st2.top())->val);
-    //         st2.pop();
-    //     }
-    //     return v;
 
+
+
+    // vector<int>v;
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     if(root==NULL) return v;
+    //     postorderTraversal(root->left);
+    //     postorderTraversal(root->right);
+    //     v.push_back(root->val);
+    //     return v;
     // }
 };
