db40025a28c0c5da926ee4c878555cc831cf4d75
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-06-04 15:37:51 +0530

    Time: 3 ms (12.08%), Space: 11.8 MB (53.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int f(int i,int j,int m,int n,vector<vector<int>> &dp,vector<vector<int>> &obstacleGrid){
+        if(i<0 || i>=m || j<0|| j>=n || obstacleGrid[i][j]==1) return 0;
         if(i==m-1 && j==n-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(i<0 || i>=m || j<0|| j>=n || obstacleGrid[i][j]==1) return 0;
+        
 
         int down=f(i+1,j,m,n,dp,obstacleGrid);
         int right=f(i,j+1,m,n,dp,obstacleGrid);
