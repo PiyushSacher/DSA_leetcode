b50e1ffedbc5f487ad36fe267d0a769228470e95
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-06-04 15:38:05 +0530

    Time: 0 ms (100%), Space: 11.9 MB (53.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int f(int i,int j,int m,int n,vector<vector<int>> &dp,vector<vector<int>> &obstacleGrid){
+        //make this line as 1st to check if the dest cell is an obstacle else all test case will not pass
         if(i<0 || i>=m || j<0|| j>=n || obstacleGrid[i][j]==1) return 0;
         if(i==m-1 && j==n-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
