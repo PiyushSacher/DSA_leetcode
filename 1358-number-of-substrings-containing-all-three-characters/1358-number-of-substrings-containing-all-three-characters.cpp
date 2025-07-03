278ad0c0e17d1e0bc07a0ad00edcbd8e685aa4e9
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-07-03 11:51:42 +0530

    Time: 14 ms (39.45%), Space: 11.1 MB (34.5%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0;i<s.size();i++){
             hash[s[i]-'a']=i;
             if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                //remember the formula
                 count+=1+min({hash[0],hash[1],hash[2]});
             }
         }
