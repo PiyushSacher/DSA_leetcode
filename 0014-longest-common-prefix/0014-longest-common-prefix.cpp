a3fc582ddc73bf52db9c7a60c0103f8fe0b227ef
Piyush Sacher <124122589+PiyushSacher@users.noreply.github.com>
2025-10-02 22:05:43 +0530

    Time: 0 ms (100%), Space: 12.1 MB (17.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        //approach 
+        //as longest common prefix would be same in all the elements of the vector,so we will sort them as when we will sort, all the equal elements will be group together
+                // after sorting
+//cluster           club
+//clue              clue
+//clutch            clumsy
+//club              cluster
+//clumsy            cluster
+
+// now see clu is coming everywhere
+//now we can just compare the first word and the last word and try to look up till where the words are common
+// suppose circle is also there , then circle would be coming at the top.
         string result="";
         int n=strs.size();
         sort(strs.begin(),strs.end());
